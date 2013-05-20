1b3d6926969dbe3460aa78396aa763f7ad3671ee
Sam Breed <sam@quickleft.com>
2013-05-19 23:20:06 -0600

    adding some comments and expanding readme

--- readme.md
+++ readme.md
@@ -3,6 +3,10 @@
 This is a simple example of how to authenticate with an oauth service
 via your own server from a Phonegap application.
 
+tl;dr - take a look at `www/js/index.js` and `views/index.ejs` to see
+the gist of what's going on here--passing back user information from an
+auth server to a phonegap client using HTML5 postMessage.
+
 ## Setup
 
 To get started, run
@@ -30,5 +34,33 @@ the server's running, build and run a client through the simulator.
 Or open the Xcode project that's located in
 `platforms/ios/HelloCordova.xcodeproj` if you're interested in seeing
 the actual console output from the app.
+
+Once the simulator is running, click the "login" link on the homepage
+and you'll be logged into the application server using oauth 2, and
+passing that information back to your phonegap app.
+
+## Next Steps
+
+First off, a note about security: this should only be done over https in
+production environments. If you're feeling paranoid, you should hash the
+data with a secret that's shared by the phonegap client app and the
+server--but you'd still be vulnerable to someone picking apart the app
+to get at it if they really wanted to since it would have to be
+implemented in JavaScript (or a cordova plugin if you got really fancy
+and platform-specific.)
+
+Keep in mind that this is exposing logged in user information with
+JavaScript, so at a minimum you should think about keeping the iframe
+url relatively obscure, explicitly filtering out non-file protocol
+iframe loads or use a querystring parameter that would be known only to
+the client and the server and filter based on that (hell, you could even
+implement oauth 2 on the signing server too.)
+
+The next step is to take the auth token that's being passed back from
+the auth server and embed it into outgoing requests. This can usually be
+done with a querystring or Authorization header by way of an
+`auth_token` (for instance, using omniauth in Ruby this works
+out-of-the-box.)
+
 
 
--- www/js/index.js
+++ www/js/index.js
@@ -9,9 +9,9 @@ var app = {
         _this.authenticate('instagram')
           .then(function(data){
             console.log(data);
-            alert(data.id);
+            window.alert('User id: '+ data.id);
           }, function(err){
-            console.log(err);
+            window.alert(err);
           });
         return false;
       });
@@ -28,40 +28,61 @@ var app = {
         var completeElem = document.querySelector('#' + id + ' .complete');
         completeElem.className = completeElem.className.split('hide').join('');
     },
-
     authenticate: function(service){
-      var baseUrl = 'http://lvh.me:3000/auth/' + service;
-      var ref = window.open(baseUrl, '_blank', 'location=yes,transitionstyle=fliphorizontal');
-      var self = this;
       var dfd = new $.Deferred();
+      // For the purposes of this demo the auth server is running locally on
+      // port 3000
+      var baseUrl = 'http://lvh.me:3000/auth/' + service;
 
+      // Open a child browser that navigates to your auth server
+      var ref = window.open(baseUrl, '_blank', 'location=yes,transitionstyle=fliphorizontal');
+
+      // Child browsers seem to not want to fire `loadend` events consistently,
+      // but the nature of the multiple-redirects involved in oauth access make
+      // it so that `loadstart` works just fine
       ref.addEventListener('loadstart', function(event) {
+        // Only process the authentication when the child browser returns to
+        // your server's redirect uri
         if( event.url !== 'http://lvh.me:3000/' ){
           return;
         }
+
+        // Dismiss the child browser
         ref.close();
 
+        // Set up a postMessage event listener
         window.addEventListener('message', function(event){
           dfd.resolve(event.data);
+          // Wait a tick before cleaning up the iframe
           setTimeout(function(){
             $(iframe).remove();
           },0);
         });
 
+        // Make an iframe that loads your auth server. By the magic of shared
+        // cookies, this iframe will *already be authenticated* with your auth
+        // server, which has JavaScript that will respond to a postMessage
+        // event with the user / auth information your app needs.
         var iframe = document.createElement('iframe');
         iframe.src = 'http://lvh.me:3000/';
 
         $(iframe).appendTo('body').load(function(){
-          iframe.contentWindow.postMessage('authenticate', 'http://lvh.me:3000/');
+          // Once the iframe is loaded, ping the page with a postMessage. This
+          // will respond by triggering the handler we attached above, giving
+          // you logged-in access to the server.
+          iframe.contentWindow.postMessage(location.href, 'http://lvh.me:3000/');
         });
       });
 
+      // This whole process should timeout after 2 minutes
       setTimeout(function(){
         if( dfd.state() !== 'resolved' ){
           dfd.reject('Authentication timed out after 2 minutes');
         }
       }, 120e3);
 
+      // This *could* very well be done without using promises, but we use them
+      // here because this gives this broad compatability with $.ajax
       return dfd.promise();
     }
 };
